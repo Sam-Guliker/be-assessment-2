95b0278789dbea3774b60e2458e11fca96effe69
Senmetsu <samguliker@hotmail.com>
2018-04-03 20:44:36 +0200

    layout setup

--- routes/404.js
+++ routes/404.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('404', function(req, res, next){
-  // trigger a 404 since no other middleware
-  // will match /404 after this one, and we're not
-  // responding here
   next();
 });
 
